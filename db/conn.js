becdb2720c17ee033f818b070a7b904a8a106f6e
Asishk9 <asishkumargupta98@gmail.com>
2023-08-18 11:04:33 +0530

    update conn.js

--- db/conn.js
+++ db/conn.js
@@ -5,4 +5,4 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB,{
     useUnifiedTopology:true,
     useNewUrlParser:true
-}).then(()=>console.log("connection is successfully done")).catch((error)=>console.log("error hai" + error.message))
+}).then(()=>console.log("database connection is successfully done")).catch((error)=>console.log("error hai" + error.message))
